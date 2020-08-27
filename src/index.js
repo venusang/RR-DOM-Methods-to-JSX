0dec43623fe3fb7c4ec7beec11a068b4568b5fcd
Joshua Miles <joshua.m@hackerusa.com>
2020-08-27 15:55:30 -0500

    v1 solution

--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
-let koalaContainer = document.createElement('div')
-
-koalaContainer.setAttribute('class', 'ui items')
-
-koalas.forEach(koala => {
-    let koalaCard = document.createElement('div')
-    koalaCard.setAttribute('class', 'item')
-    koalaCard.style.cursor = 'pointer';
-
-    // Koala Image
-    let imageContainer = document.createElement('div')
-    let koalaImage = document.createElement('img')
-
-    imageContainer.setAttribute('class', 'image')
-
-    koalaImage.setAttribute('src', koala.imageURL)
-
-    imageContainer.append(koalaImage)
-
-    // Koala Card Content
-    let koalaContent = document.createElement('div')
-    koalaContent.setAttribute('class', 'content')
-
-    let header = document.createElement('div')
-    header.setAttribute('class', 'header')
-    header.append(koala.name)
-
-    let descriptionParagraph = document.createElement('p')
-    descriptionParagraph.append(koala.description)
-
-    let descriptionContainer = document.createElement('div')
-    descriptionContainer.setAttribute('class', 'description')
-    descriptionContainer.append(descriptionParagraph)
-
-    koalaContent.append(
-        header,
-        descriptionContainer
-    )
-
-    koalaCard.append(
-        imageContainer,
-        koalaContent
-    )
-
-    koalaContainer.append(koalaCard)
-})
+let koalaContainer = (
+    <div className="ui items">
+        {koalas.map( koala => (
+            <div className="item" style={{ cursor: 'pointer' }}>
+                <div className="image" >
+                    <img src={koala.imageURL} />
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {koala.name}
+                    </div>
+                    <div className="description">
+                        <p>{koala.description}</p>
+                    </div>
+                </div>
+            </div>
+        ))}
+    </div>
+)
 
 document.body.append(koalaContainer)
